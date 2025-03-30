ac9f3d5d86f207d622f9a1f0da9d662f4c5da0d0
polashchandra <polashchandra210@gmail.com>
2025-03-30 19:31:10 +0600

    problrm5 solved

--- Problem6.ts
+++ Problem6.ts
@@ -7,5 +7,6 @@ interface Profile {
   function updateProfile(profile: Profile, updates: Partial<Profile>): Profile {
     return { ...profile, ...updates };
   }
+  
   const myProfile = { name: "musa", age: 25, email: "musa@564gmail.com" };
   console.log(updateProfile(myProfile, { age: 26 }));
